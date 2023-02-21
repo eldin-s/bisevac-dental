1f866d87b6d62286ed779e16c64d0f0798196749
Eldin-S <eldin_s12@hotmail.com>
2023-02-21 16:14:50 +0100

    Slider update and funcionality

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 
 export const Slider = () => {
+  // const slides = [
+  //   {
+  //     subheading: 'Online Rešenje',
+  //   },
+  // ];
+
   const [slide, setSlide] = useState(true);
 
   const setSlideTo0 = () => {
@@ -19,18 +25,32 @@ export const Slider = () => {
 
           <div className="slider-information-right">
             <h5
-              className={`slide-subheading ${slide ? 'animateToRight0s' : ''}`}
+              className={
+                slide ? 'slide-subheading animateToRight0s' : 'slide-subheading'
+              }
             >
               Online Rešenje
             </h5>
-            <h1 className={`slide-heading ${slide ? 'animateToRight05s' : ''}`}>
+            <h1
+              className={`slide-heading ${
+                slide ? 'slide-heading animateToRight05s' : 'slide-heading'
+              }`}
+            >
               Posvećeni pouzdanoj stomatološkoj nezi
             </h1>
-            <p className={`slide-text ${slide ? 'animateToRight15s' : ''}`}>
+            <p
+              className={`${
+                slide ? 'slide-text animateToRight15s' : 'slide-text'
+              }`}
+            >
               Brinemo o rastućim potrebama naše zajednice
             </p>
 
-            <div className={`slide-info ${slide ? 'animateToRight2s' : ''}`}>
+            <div
+              className={`${
+                slide ? 'slide-info animateToRight2s' : 'slide-info'
+              }`}
+            >
               <button className="btn btn-kontakt">Kontakt</button>
               <p className="slide-info-text">Dobijte besplatan pregled</p>
             </div>
@@ -42,28 +62,38 @@ export const Slider = () => {
 
           <div className="slider-information-right">
             <h5
-              className={`slide-subheading slide2 ${
-                slide ? '' : 'animateToRight0s'
+              className={`${
+                slide
+                  ? 'slide-subheading slide2'
+                  : 'slide-subheading slide2 animateToRight0s'
               }`}
             >
               Profesionalno Osoblje
             </h5>
             <h1
-              className={`slide-heading light ${
-                !slide ? 'animateToRight05s' : ''
+              className={` ${
+                slide
+                  ? 'slide-heading light'
+                  : 'slide-heading light animateToRight05s'
               }`}
             >
               Vaš osmeh je <br /> Naš priorotet
             </h1>
             <p
-              className={`slide-text light ${
-                !slide ? 'animateToRight15s' : ''
+              className={`${
+                slide
+                  ? 'slide-text light'
+                  : 'slide-text light animateToRight15s'
               }`}
             >
               Personalizovane i visoko-kvalitetne usluge
             </p>
 
-            <div className={`slide-info ${slide ? '' : 'animateToRight2s'}`}>
+            <div
+              className={`${
+                slide ? 'slide-info' : 'slide-info animateToRight2s'
+              }`}
+            >
               <button className="btn btn-kontakt">Kontakt</button>
             </div>
           </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-// import { Slider1 } from '../components/Slider1';
-import { Slider } from '../components/Slider';
+import { Slider1 } from '../components/Slider1';
+// import { Slider } from '../components/Slider';
 
 import './home.css';
 
 export const Home = () => {
   return (
     <div className="bisevac-header">
-      <Slider />
+      {/* <Slider /> */}
+      <Slider1 />
     </div>
   );
 };
